1048c1599913c4e5e92a76552a29dc96e9f6cca3
aminnateghi10 <aminnateghi10@gmail.com>
2022-08-09 22:35:59 +0430

    items in side menu

--- src/Component/admin/Component/Admin.jsx
+++ src/Component/admin/Component/Admin.jsx
@@ -1,15 +1,25 @@
 import React, {useEffect} from "react";
 import {BrowserRouter,Routes , Route , useNavigate } from "react-router-dom";
 import '../css/style.css'
-import SideMenu from "./SideMenu.jsx";
-import Tapbar from "./Tapbar.jsx";
-import Comments from './Comments.jsx'
-import Contentwrapper from "./Contentwrapper.jsx";
-import Service from "./Service.jsx";
-import Experience from './Experience.jsx'
-import Clients from "./Clients.jsx";
-import Skill from "./Skill.jsx";
-import Information from "./Information.jsx";
+import loadable from '@loadable/component'
+const SideMenu = loadable(() => import('./SideMenu.jsx'))
+const Tapbar = loadable(() => import('./Tapbar.jsx'))
+const Comments = loadable(() => import('./Comments.jsx'))
+const Contentwrapper = loadable(() => import('./Contentwrapper.jsx'))
+const Service = loadable(() => import('./Service.jsx'))
+const Experience = loadable(() => import('./Experience.jsx'))
+const Clients = loadable(() => import('./Clients.jsx'))
+const Skill = loadable(() => import('./Skill.jsx'))
+const Information = loadable(() => import('./Information.jsx'))
+// import SideMenu from "./SideMenu.jsx";
+// import Tapbar from "./Tapbar.jsx";
+// import Comments from './Comments.jsx'
+// import Contentwrapper from "./Contentwrapper.jsx";
+// import Service from "./Service.jsx";
+// import Experience from './Experience.jsx'
+// import Clients from "./Clients.jsx";
+// import Skill from "./Skill.jsx";
+// import Information from "./Information.jsx";
 function Admin(){
     let navigate = useNavigate()
     useEffect(()=>{
--- src/Component/admin/Component/SideMenu.jsx
+++ src/Component/admin/Component/SideMenu.jsx
@@ -3,53 +3,105 @@ import {AiOutlineHome, AiOutlineLeft, AiOutlineMessage, AiOutlineSetting} from "
 import {Link} from "react-router-dom";
 
 function SideMenu() {
-
+    // const items = [
+    //     { name: "home", label: "df", Icon: HomeIcon },
+    //     {
+    //         name: "billing",
+    //         label: "Billing",
+    //         Icon: ReceiptIcon,
+    //         items: [
+    //             { name: "statements", label: "Statements", onClick },
+    //             { name: "reports", label: "Reports", onClick }
+    //         ]
+    //     },
+    //     "divider",
+    //     {
+    //         name: "settings",
+    //         label: "Settings",
+    //         Icon: SettingsIcon,
+    //         items: [
+    //             { name: "profile", label: "Profile" },
+    //             { name: "insurance", label: "Insurance", onClick },
+    //             "divider",
+    //             {
+    //                 name: "notifications",
+    //                 label: "Notifications",
+    //                 Icon: NotificationsIcon,
+    //                 items: [
+    //                     { name: "email", label: "Email", onClick },
+    //                     {
+    //                         name: "desktop",
+    //                         label: "Desktop",
+    //                         Icon: DesktopWindowsIcon,
+    //                         items: [
+    //                             { name: "schedule", label: "Schedule" },
+    //                             { name: "frequency", label: "Frequency" },
+    //                             {
+    //                                 name: 'amin',
+    //                                 label: 'desktopin',
+    //                                 Icon: DesktopWindowsIcon,
+    //                                 items:[
+    //                                     { name: "schedule", label: "Schedule" },
+    //                                     { name: "frequency", label: "Frequency" }
+    //                                 ]
+    //                             }
+    //                         ]
+    //                     },
+    //                     { name: "sms", label: "SMS" }
+    //                 ]
+    //             }
+    //         ]
+    //     }
+    // ];
     return (
-        <div className="left side-menu">
-            <button type="button" className="button-menu-mobile button-menu-mobile-topbar open-left waves-effect">
-                <i className="mdi mdi-close"></i>
-            </button>
-            <div className="left-side-logo d-block d-lg-none">
-                <div className="text-center">
-                    <a href="index.html" className="logo">
-                        <img src="assets/images/logo_dark.png" height="20" alt="logo"/>
-                    </a>
-                </div>
-            </div>
-            <div className="sidebar-inner slimscrollleft">
-                <div id="sidebar-menu">
-                    <ul>
-                        <li>
-                            <Link to='/admin/comments/dashboard' className="waves-effect">
-                                <AiOutlineHome size={20}/>
-                                <span> داشبورد</span>
-                            </Link>
-                        </li>
-
-                        <li  className={`has_sub nav-active`}>
-                            <a>
-                                <span> پیام ها </span>
-                                <span className="menu-arrow float-right"><AiOutlineLeft size={18}/></span>
-                            </a>
-                            <ul className="list-unstyled">
-                                <li><Link to='/admin/comments'>صندوق ورودی</Link></li>
-                            </ul>
-                        </li>
-                        <li className='has_sub nav-active'>
-                            <a className="waves-effect"><AiOutlineSetting size={20}/> <span> مدیریت / ویرایش </span>
-                                <span className="menu-arrow float-right"><AiOutlineLeft size={18}/> </span></a>
-                            <ul className="list-unstyled">
-                                <li><Link to='/admin/information'>اطلاعات</Link></li>
-                                <li><Link to='/admin/skill'>مهارت ها</Link></li>
-                                <li><Link to='/admin/service'>سرویس ها</Link></li>
-                                <li><Link to='/admin/experience'>تجربیات</Link></li>
-                                <li><Link to='/admin/clients'>مشتریان و نظرات</Link></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <>
+            AMN
+        </>
+        // <div className="left side-menu">
+        //     <button type="button" className="button-menu-mobile button-menu-mobile-topbar open-left waves-effect">
+        //         <i className="mdi mdi-close"></i>
+        //     </button>
+        //     <div className="left-side-logo d-block d-lg-none">
+        //         <div className="text-center">
+        //             <a href="index.html" className="logo">
+        //                 <img src="assets/images/logo_dark.png" height="20" alt="logo"/>
+        //             </a>
+        //         </div>
+        //     </div>
+        //     <div className="sidebar-inner slimscrollleft">
+        //         <div id="sidebar-menu">
+        //             <ul>
+        //                 <li>
+        //                     <Link to='/admin/comments/dashboard' className="waves-effect">
+        //                         <AiOutlineHome size={20}/>
+        //                         <span> داشبورد</span>
+        //                     </Link>
+        //                 </li>
+        //
+        //                 <li  className={`has_sub nav-active`}>
+        //                     <a>
+        //                         <span> پیام ها </span>
+        //                         <span className="menu-arrow float-right"><AiOutlineLeft size={18}/></span>
+        //                     </a>
+        //                     <ul className="list-unstyled">
+        //                         <li><Link to='/admin/comments'>صندوق ورودی</Link></li>
+        //                     </ul>
+        //                 </li>
+        //                 <li className='has_sub nav-active'>
+        //                     <a className="waves-effect"><AiOutlineSetting size={20}/> <span> مدیریت / ویرایش </span>
+        //                         <span className="menu-arrow float-right"><AiOutlineLeft size={18}/> </span></a>
+        //                     <ul className="list-unstyled">
+        //                         <li><Link to='/admin/information'>اطلاعات</Link></li>
+        //                         <li><Link to='/admin/skill'>مهارت ها</Link></li>
+        //                         <li><Link to='/admin/service'>سرویس ها</Link></li>
+        //                         <li><Link to='/admin/experience'>تجربیات</Link></li>
+        //                         <li><Link to='/admin/clients'>مشتریان و نظرات</Link></li>
+        //                     </ul>
+        //                 </li>
+        //             </ul>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
 
